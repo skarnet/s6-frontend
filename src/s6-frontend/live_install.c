c2740b983f6bb4baf0d5a1772fa3682cc66aa5f9
Laurent Bercot <ska-skaware@skarnet.org>
2026-01-18 02:50:51 +0000

    Add s6 live install --init ; finish and fix documentation

--- src/s6-frontend/live_install.c
+++ src/s6-frontend/live_install.c
@@ -12,6 +12,7 @@ enum golb_e
 {
   GOLB_BLOCK = 0x01,
   GOLB_KEEPOLD = 0x02,
+  GOLB_INIT = 0x04,
 } ;
 
 enum gola_e
@@ -26,6 +27,7 @@ void live_install (char const *const *argv)
   {
     { .so = 'b', .lo = "block", .clear = 0, .set = GOLB_BLOCK },
     { .so = 'K', .lo = "keep-old", .clear = 0, .set = GOLB_KEEPOLD },
+    { .so = 0, .lo = "init", .clear = 0, .set = GOLA_INIT },
   } ;
   static gol_arg const rgola[] =
   {
@@ -34,7 +36,7 @@ void live_install (char const *const *argv)
   uint64_t wgolb = 0 ;
   unsigned int m = 0 ;
   char const *wgola[GOLA_N] = { 0 } ;
-  char const *newargv[16] ;
+  char const *newargv[17] ;
   char fmtv[UINT_FMT] ;
 
   argv += GOL_argv(argv, rgolb, rgola, &wgolb, wgola) ;
@@ -60,6 +62,7 @@ void live_install (char const *const *argv)
     newargv[m++] = "-f" ;
     newargv[m++] = wgola[GOLA_CONVFILE] ;
   }
+  if (wgolb & GOLB_INIT) newargv[m++] = "--no-update" ;
   newargv[m++] = "--" ;
   newargv[m++] = "current" ;
   newargv[m++] = 0 ;
